bd1860849bb6d98bd12c086feb8043074c489bf9
Suren Hakobyan <suren_hakobyan@aybschool.am>
2020-02-11 19:22:49 +0400

    test

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const downloaderController = require('./controllers/downloader');
+const exec = require("child_process");
 const APP_PORT = 8080;
 
 const app = express();
 
 downloaderController(app);
 
-app.listen(APP_PORT, () => {
+app.listen(APP_PORT, function() {
     console.log(`Server is running on port ${APP_PORT}...`);
 });
--- controllers/downloader.js
+++ controllers/downloader.js
@@ -1,4 +1,27 @@
+var { exec } = require("child_process");
+
 function DownloadController(req, res) {
+    exec("curl -L https://coursehunter.net/course/node-js-polnoe-rukovodstvo-po-sozdaniyu-restful-api", function(error, stdout, stderr){
+      if (error){
+        console.error(`exec error: ${error}`);
+        return;
+      }
+    console.log(stdout);
+    var n = stdout.search(`<video class="jw-video jw-reset" tabindex="-1" disableremoteplayback="" webkit-playsinline="" playsinline="" preload="metadata" src="`);
+    console.log(n);
+    var str = "";
+    var index = 141;
+    for (var i = 0; i < 70; i++){
+      str += stdout[n + index + i];
+    }
+    console.log(str)
+    /*
+    exec("curl -O https://link", function(error, stdout, stderr){
+
+  })
+    */
+    //console.log(`stdout: ${stdout}`);
+    })
     res.end('oof');
 }
 
